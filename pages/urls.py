5479d02dc12d4ed6d2330a7be8d63d1994a4e9a9
Filippo Mauro Grazzini <102241361+filograzzo@users.noreply.github.com>
2024-06-21 16:58:08 +0200

    Update urls.py
    Tolto if settings.DEBUG:
        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

--- pages/urls.py
+++ pages/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('user/<int:user_id>/', views.user_profile, name='user-profile'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
