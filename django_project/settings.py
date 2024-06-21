dcf6ed55be93d6b58b6ce68fc26011e8fb946df0
Filippo Mauro Grazzini <102241361+filograzzo@users.noreply.github.com>
2024-06-21 16:35:17 +0200

    Update settings.py
    da Path(__file__).resolve().parent.parent a Path(__file__).resolve()

--- django_project/settings.py
+++ django_project/settings.py
@@ -16,7 +16,7 @@ from django.contrib.messages import constants as messages
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
